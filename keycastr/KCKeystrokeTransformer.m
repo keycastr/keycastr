5b2d56f3547bfb394a794d49e0c214f0663be82c
Andrew Kitchen <akitchen@gmail.com>
2022-11-14 12:13:18 -0800

    Remove NSNum macro and update formatting
    Keeping legacy NSDictionary style for now due to inline comments, etc.

--- keycastr/KCKeystrokeTransformer.m
+++ keycastr/KCKeystrokeTransformer.m
@@ -49,16 +49,15 @@ static NSString* kShiftKeyString = @"\xe2\x87\xa7";
 static NSString* kLeftTabString = @"\xe2\x87\xa4";
 
 #define UTF8(x) [NSString stringWithUTF8String:x]
-#define NSNum(x) [NSNumber numberWithInt:x]
 
 @synthesize keyboardLayout = _keyboardLayout;
 
-+(BOOL) allowsReverseTransformation
++ (BOOL)allowsReverseTransformation
 {
 	return NO;
 }
 
-+(Class) transformedValueClass
++ (Class)transformedValueClass
 {
 	return [NSString class];
 }
@@ -99,65 +98,64 @@ static NSString* kLeftTabString = @"\xe2\x87\xa4";
     [super dealloc];
 }
 
--(NSDictionary*) _specialKeys
+- (NSDictionary *)_specialKeys
 {
-	static NSDictionary *d = nil;
-	if (d == nil)
-	{
-		d = [[NSDictionary alloc] initWithObjectsAndKeys:
-			UTF8("\xe2\x87\xa1"), NSNum(126), // up
-			UTF8("\xe2\x87\xa3"), NSNum(125), // down
-			UTF8("\xe2\x87\xa2"), NSNum(124), // right
-			UTF8("\xe2\x87\xa0"), NSNum(123), // left
-			UTF8("\xe2\x87\xa5"), NSNum(48), // tab
-			UTF8("\xe2\x8e\x8b"), NSNum(53), // escape
-			UTF8("\xe2\x8c\xa7"), NSNum(71), // clear
-			UTF8("\xe2\x8c\xab"), NSNum(51), // delete
-			UTF8("\xe2\x8c\xa6"), NSNum(117), // forward delete
-			UTF8("\xe2\x83\x9d"), NSNum(114), // help
-			UTF8("\xe2\x86\x96"), NSNum(115), // home
-			UTF8("\xe2\x86\x98"), NSNum(119), // end
-			UTF8("\xe2\x87\x9e"), NSNum(116), // pgup
-			UTF8("\xe2\x87\x9f"), NSNum(121), // pgdn
-			UTF8("\xe2\x86\xa9"), NSNum(36), // return
-			UTF8("\xe2\x86\xa9"), NSNum(76), // numpad enter
-			UTF8("F1"), NSNum(122), // F1
-			UTF8("F2"), NSNum(120), // F2
-			UTF8("F3"), NSNum(99),  // F3
-			UTF8("F4"), NSNum(118), // F4
-			UTF8("F5"), NSNum(96),  // F5
-			UTF8("F6"), NSNum(97),  // F6
-			UTF8("F7"), NSNum(98),  // F7
-			UTF8("F8"), NSNum(100), // F8
-			UTF8("F9"), NSNum(101), // F9
-			UTF8("F10"), NSNum(109), // F10
-			UTF8("F11"), NSNum(103), // F11
-			UTF8("F12"), NSNum(111), // F12
-			UTF8("F13"), NSNum(105), // F13
-			UTF8("F14"), NSNum(107), // F14
-			UTF8("F15"), NSNum(113), // F15
-			UTF8("F16"), NSNum(106), // F16
-			UTF8("F17"), NSNum(64), // F17
-			UTF8("F18"), NSNum(79), // F18
-			UTF8("F19"), NSNum(80), // F19
-			UTF8("F20"), NSNum(90), // F20
-			UTF8("\xe2\x90\xa3\xe2\x80\x8b"), @49, // space
-            UTF8("\xf0\x9f\x94\x85"), @145, // low brightness
-            UTF8("\xf0\x9f\x94\x86"), @144, // high brightness
-            UTF8("\xf0\x9f\x96\xa5"), @160, // mission control
-            UTF8("\xf0\x9f\x9a\x80"), @131, // launcher
-            UTF8("\xF0\x9F\x94\x8D"), @177, // spotlight key (left-pointing magnifying glass)
-            UTF8("\xF0\x9F\x8E\xA4"), @176, // dictation key (microphone)
-            UTF8("\xe2\x8f\xbe"), @178, // focus key (unicode power sleep symbol)
-            @"fn", @179, // fn key (could also be globe symbol on newer keyboards)
-            @"英数", @0x66, // eisū key, JIS keyboards only
-            @"かな", @0x68, // kana key, JIS keyboards only
-			nil];
-	}
-	return d;
+    static NSDictionary *d = nil;
+    if (d == nil) {
+        d = [[NSDictionary alloc] initWithObjectsAndKeys:
+             UTF8("\xe2\x87\xa1"), @126, // up
+             UTF8("\xe2\x87\xa3"), @125, // down
+             UTF8("\xe2\x87\xa2"), @124, // right
+             UTF8("\xe2\x87\xa0"), @123, // left
+             UTF8("\xe2\x87\xa5"), @48,  // tab
+             UTF8("\xe2\x8e\x8b"), @53,  // escape
+             UTF8("\xe2\x8c\xa7"), @71,  // clear
+             UTF8("\xe2\x8c\xab"), @51,  // delete
+             UTF8("\xe2\x8c\xa6"), @117, // forward delete
+             UTF8("\xe2\x83\x9d"), @114, // help
+             UTF8("\xe2\x86\x96"), @115, // home
+             UTF8("\xe2\x86\x98"), @119, // end
+             UTF8("\xe2\x87\x9e"), @116, // pgup
+             UTF8("\xe2\x87\x9f"), @121, // pgdn
+             UTF8("\xe2\x86\xa9"), @36,  // return
+             UTF8("\xe2\x86\xa9"), @76,  // numpad enter
+             UTF8("\xf0\x9f\x94\x85"), @145, // low brightness
+             UTF8("\xf0\x9f\x94\x86"), @144, // high brightness
+             UTF8("\xf0\x9f\x96\xa5"), @160, // mission control
+             UTF8("\xf0\x9f\x9a\x80"), @131, // launcher
+             UTF8("\xf0\x9f\x94\x8d"), @177, // spotlight key (left-pointing magnifying glass)
+             UTF8("\xf0\x9f\x8e\xa4"), @176, // dictation key (microphone)
+             UTF8("\xe2\x8f\xbe"), @178,     // focus key (unicode power sleep symbol)
+             UTF8("\xe2\x90\xa3\xe2\x80\x8b"), @49, // space
+             @"fn", @179,  // fn key (could also be globe symbol on newer keyboards)
+             @"F1", @122,  // F1
+             @"F2", @120,  // F2
+             @"F3", @99,   // F3
+             @"F4", @118,  // F4
+             @"F5", @96,   // F5
+             @"F6", @97,   // F6
+             @"F7", @98,   // F7
+             @"F8", @100,  // F8
+             @"F9", @101,  // F9
+             @"F10", @109, // F10
+             @"F11", @103, // F11
+             @"F12", @111, // F12
+             @"F13", @105, // F13
+             @"F14", @107, // F14
+             @"F15", @113, // F15
+             @"F16", @106, // F16
+             @"F17", @64,  // F17
+             @"F18", @79,  // F18
+             @"F19", @80,  // F19
+             @"F20", @90,  // F20
+             @"英数", @0x66, // eisū key, JIS keyboards only
+             @"かな", @0x68, // kana key, JIS keyboards only
+             nil];
+    }
+    return d;
 }
 
--(id) transformedValue:(id)value
+- (id)transformedValue:(id)value
 {
 	KCKeystroke *keystroke = (KCKeystroke *)value;
 	NSMutableString *mutableResponse = [NSMutableString string];
