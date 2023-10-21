e64a870377a548e1b184278c258ea761c95b2d8d
Andrew Kitchen <akitchen@gmail.com>
2023-10-21 12:57:34 -0700

    Update a number of copyright headers

--- keycastr/KCEventTap.m
+++ keycastr/KCEventTap.m
@@ -1,7 +1,8 @@
 //	Copyright (c) 2009 Stephen Deken
-//	Modifications Copyright (c) 2023 Andrew Kitchen
+//	Copyright (c) 2014-2023 Andrew Kitchen
+//
 //	All rights reserved.
-// 
+//
 //	Redistribution and use in source and binary forms, with or without modification,
 //	are permitted provided that the following conditions are met:
 //
