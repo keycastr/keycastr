adcc0e431473fc7074028007dcda7151d08f0bfd
Andrew Kitchen <akitchen@gmail.com>
2026-07-23 16:30:01 -0700

    Add contributor copyright headers to Minimal visualizer files
    Daniel Costa (SoCuul, #341) and Andrew Kitchen substantially reworked these files
    in 2026; add their copyright lines below Colin Gray's original.

--- keycastr/MinimalVisualizer.m
+++ keycastr/MinimalVisualizer.m
@@ -1,4 +1,6 @@
 //  Copyright (c) 2023 Colin Gray
+//  Copyright (c) 2026 Daniel Costa
+//  Copyright (c) 2026 Andrew Kitchen
 //  All rights reserved.
 //
 //  Redistribution and use in source and binary forms, with or without modification,
